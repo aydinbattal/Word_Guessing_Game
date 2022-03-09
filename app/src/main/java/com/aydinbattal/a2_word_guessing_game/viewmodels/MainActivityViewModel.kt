72bf47d397db8401541ec74e92f3299d5d34189d
Aydin Battal <abattal35@gmail.com>
2022-03-08 20:50:39 -0500

    Implemented the view model for main activity.

---
+++ app/src/main/java/com/aydinbattal/a2_word_guessing_game/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,27 @@
+package com.aydinbattal.a2_word_guessing_game.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.aydinbattal.a2_word_guessing_game.models.Guess
+
+/**
+ * A2-Word-Guessing-Game created by aydin
+ * student ID : 991521740
+ * on 2022-03-08 */
+class MainActivityViewModel(val db: RoomDatabase): ViewModel() {
+    var secretWord:String = "CABLE"
+    var guessList:MutableList<Guess> = mutableListOf()
+    var isGameOver:Boolean = false
+    //var isGameOver = MutableLiveData<Boolean>(false)
+
+    fun saveGame(){
+        //todo
+    }
+
+    fun secretWord(userGuess:Guess){
+        if (userGuess.userGuess == userGuess.secretWord || userGuess.chanceId > 5){
+            isGameOver
+        }
+        guessList.add(userGuess)
+    }
+}
