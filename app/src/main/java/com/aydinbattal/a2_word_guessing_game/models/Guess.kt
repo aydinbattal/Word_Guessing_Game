04f645564efeea4506e32dae160c4ffd28187c23
Aydin Battal <abattal35@gmail.com>
2022-03-08 19:32:21 -0500

    Implemented Guess class.

---
+++ app/src/main/java/com/aydinbattal/a2_word_guessing_game/models/Guess.kt
@@ -0,0 +1,23 @@
+package com.aydinbattal.a2_word_guessing_game.models
+
+/**
+ * A2-Word-Guessing-Game created by aydin
+ * student ID : 991521740
+ * on 2022-03-08 */
+data class Guess(val chanceId:Int, val secretWord:String, val userGuess:String) {
+
+    fun getLetterStates():List<Result>{
+        val resultList:MutableList<Result> = mutableListOf()
+        userGuess.forEach {
+            if (it in secretWord && userGuess.indexOf(it) == secretWord.indexOf(it)){
+                resultList.add(Result.CORRECT)
+            } else if (it in secretWord && userGuess.indexOf(it) != secretWord.indexOf(it)) {
+                resultList.add(Result.WRONG_POSITION)
+            } else {
+                resultList.add(Result.NOT_IN_WORD)
+            }
+        }
+
+        return resultList.toList()
+    }
+}
