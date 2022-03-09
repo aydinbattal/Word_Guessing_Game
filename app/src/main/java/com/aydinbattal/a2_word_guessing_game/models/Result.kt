f08de0939effcaf2c8069d41f0851f1611dccd0c
Aydin Battal <abattal35@gmail.com>
2022-03-08 19:38:45 -0500

    Implemented Result enum class.

---
+++ app/src/main/java/com/aydinbattal/a2_word_guessing_game/models/Result.kt
@@ -0,0 +1,9 @@
+package com.aydinbattal.a2_word_guessing_game.models
+
+/**
+ * A2-Word-Guessing-Game created by aydin
+ * student ID : 991521740
+ * on 2022-03-08 */
+enum class Result(val color: String) {
+    CORRECT("#FFC20A"), WRONG_POSITION("#0C7BDC"), NOT_IN_WORD("#FFFFFF")
+}
